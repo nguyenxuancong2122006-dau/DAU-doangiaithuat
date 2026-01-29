560b2b38167d03310701716381dd4594a6c48fc2
DAU-doangiaithuat <nguyenxuancong2122006@gmail.com>
2026-01-29 21:14:12 +0700

    code

---
+++ 1. Two Sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i in range(len(nums)):
+           need = target - nums[i]
+           if need in seen:
+               return [seen[need],i]
+           seen[nums[i]] = i
